f5654137db82f25d2a95b224cf7813a71097dbd2
sicarde <camille.koziar@gmail.com>
2015-10-09 00:09:49 +0200

    some stuff from G_Rand who currently doesn't do anything

---
+++ OpenglTries/GPUBuffer.hh
@@ -0,0 +1,33 @@
+#ifndef GPUBUFFER_H_
+# define GPUBUFFER_H_
+#include <string>
+#include <vector>
+#include "uniform.hh"
+#include <GL/glew.h>
+#include <assimp/Importer.hpp>
+
+class GPUBuffer {
+    private:
+	GLuint _vbo;
+	GLuint _ebo;
+	bool _hasNormals;
+	bool _hasTexture;
+	std::vector<GLfloat> _vertexArray;
+	std::vector<GLuint> _elementArray;
+    public:
+	explicit GPUBuffer();
+	GPUBuffer(const GPUBuffer&);
+	~GPUBuffer();
+	void operator=(const GPUBuffer&);
+	inline decltype(_vertexArray)& getVertexArray() { return _vertexArray; }
+	inline decltype(_elementArray)& getElementArray() { return _elementArray; }
+	inline void setHasNormals(bool&& b_) { _hasNormals = b_; }
+	inline void setHasTexture(bool&& b_) { _hasTexture = b_; }
+	void setBuffer();
+	void regenVboEbo();
+	void GPUFree();
+	void CPUFree();
+	void draw(GLenum drawStyle_)const noexcept;
+};
+
+#endif /* !GPUBUFFER_H_ */
---
+++ OpenglTries/material.hh
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <array>
+#include "uniform.hh"
+#include "texture.hh"
+#include "shader.hh"
+
+class Material {
+    private:
+	static const char* const _StexStringArray_[16];
+	GLuint _shaderProgram;
+	GLuint _samplerArrayLocation;
+	Uniform<unsigned int> _uTextureAmount;
+	std::vector<Shader> _shaders;
+	std::vector<const Texture*> _textures;
+    public:
+	Material();
+	~Material();
+	void addShader(GLenum, const std::string&) noexcept;
+	void link()noexcept;
+	void use()const noexcept;
+	void compileAll()noexcept;
+	decltype(_textures)::const_iterator addTexture(Texture*);
+	void excludeTexture(decltype(_textures)::iterator);
+	template <typename T> Uniform<T> getUniform(const std::string&);
+	inline decltype(_textures)& getTextureList();
+	inline GLuint getShaderProgram()const {
+	    return _shaderProgram;
+	}
+
+};
+
+template <typename T> Uniform<T> Material::getUniform(const std::string& name) {
+    return Uniform<T>(T(), _shaderProgram, name);
+}
