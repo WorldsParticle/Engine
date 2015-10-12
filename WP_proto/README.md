f10ee16ea83327eff7b2cb361aabc354bf09c748
Louis Martin-Pierrat <louis.martin-pierrat@epitech.eu>
2015-10-13 00:51:35 +0200

    modification CMakeList.txt, add README.md and FindAssimp linux support modification

---
+++ WP_proto/README.md
@@ -0,0 +1,12 @@
+# worldsparticle
+Utilisation d'OpenGL 4+ avec Qt
+
+# Compilation
+
+```shell
+mkdir cmake
+cd cmake
+cmake -DCMAKE_BUILD_TYPE=release ..
+make
+```
+
--- WP_proto/cmake/cmake_modules/FindAssimp.cmake
+++ WP_proto/cmake/cmake_modules/FindAssimp.cmake
@@ -8,36 +8,38 @@ SET(ASSIMP_POSSIBLE_INCDIR_SUFFIXES
  "includes"
 )
 SET(ASSIMP_POSSIBLE_LIB_SUFFIXES
- #"lib32"
+ "lib32"
  "lib64"
 )
 
 
 
 FIND_PATH(
-  assimp_INCLUDE_DIRS
-  NAMES postprocess.h scene.h version.h config.h cimport.h
-  PATHS ${ASSIMP_POSSIBLE_PATH}
-  PATH_SUFFIXES ${ASSIMP_POSSIBLE_INCDIR_SUFFIXES}
-)
+    ASSIMP_INCLUDE_DIRS
+    NAMES postprocess.h scene.h version.h config.h cimport.h
+    PATHS ${ASSIMP_POSSIBLE_PATH}
+    PATH_SUFFIXES ${ASSIMP_POSSIBLE_INCDIR_SUFFIXES}
+    )
 
 FIND_LIBRARY(
-  assimp_LIBRARIES
-  NAMES assimp
-  PATHS ${ASSIMP_POSSIBLE_PATH}
-  PATH_SUFFIXES ${ASSIMP_POSSIBLE_LIB_SUFFIXES}
-)
+    ASSIMP_LIBRARIES
+    NAMES assimp
+    PATHS ${ASSIMP_POSSIBLE_PATH}
+    PATH_SUFFIXES ${ASSIMP_POSSIBLE_LIB_SUFFIXES}
+    )
 
 IF (assimp_INCLUDE_DIRS AND assimp_LIBRARIES)
-  SET(assimp_FOUND TRUE)
+    SET(assimp_FOUND TRUE)
 ENDIF (assimp_INCLUDE_DIRS AND assimp_LIBRARIES)
 
 IF (assimp_FOUND)
-  IF (NOT assimp_FIND_QUIETLY)
-    MESSAGE(STATUS "Found asset importer library: ${assimp_LIBRARIES}")
-  ENDIF (NOT assimp_FIND_QUIETLY)
+    IF (NOT assimp_FIND_QUIETLY)
+        MESSAGE(STATUS "Found asset importer library: ${assimp_LIBRARIES}")
+    ENDIF (NOT assimp_FIND_QUIETLY)
 ELSE (assimp_FOUND)
-  IF (assimp_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Could not find asset importer library")
-  ENDIF (assimp_FIND_REQUIRED)
+    IF (assimp_FIND_REQUIRED)
+        MESSAGE(FATAL_ERROR "Could not find asset importer library")
+    ENDIF (assimp_FIND_REQUIRED)
 ENDIF (assimp_FOUND)
+
+message(${assimp_LIBRARIES})
