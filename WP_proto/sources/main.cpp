e0abce9ed90e97cc06015e96e112d208a43bee8d
Louis Martin-Pierrat <louis.martin-pierrat@epitech.eu>
2015-10-14 22:46:54 +0200

    add headers for main.cpp and worldparticles.hpp.in

--- WP_proto/sources/main.cpp
+++ WP_proto/sources/main.cpp
@@ -1,11 +1,22 @@
-#include "window/mainwindow.h"
-#include <QApplication>
+///
+/// \file main.cpp
+/// \brief TODO
+/// \author Martin-Pierrat Louis (mart_p)
+/// \version 1.0.0
+/// \date October 13, 2015
+///
+/// TODO
+///
 
-int main(int argc, char *argv[])
+#include    "mainwindow.h"
+
+#include    <QApplication>
+
+int     main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication    a(argc, argv);
+    MainWindow      w;
 
+    w.show();
     return a.exec();
 }
