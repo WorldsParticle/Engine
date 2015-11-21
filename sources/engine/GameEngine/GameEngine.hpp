20a3cd96f84ec5ecb7f486c430903f6470054885
Martin-Pierrat Louis <louis.martin-pierrat@epitech.eu>
2015-11-21 04:49:47 +0100

    Merge conflict resolved

--- sources/engine/GameEngine/GameEngine.hpp
+++ sources/engine/GameEngine/GameEngine.hpp
@@ -11,15 +11,6 @@
 #ifndef GAMEENGINE_H
 #define GAMEENGINE_H
 
-<<<<<<< HEAD:sources/engine/GameEngine/GameEngine.hpp
-=======
-#include <string>
-#include "model.h"
-#include "myglpainter.h"
-#include "mesh.hh"
-#include "camera.h"
-
->>>>>>> origin/master:sources/engine/gameengine.h
 #include    "worldparticles.hpp"
 
 #include    "GameView.hpp"
@@ -33,15 +24,8 @@ public:
     void    draw();
 
 private:
-<<<<<<< HEAD:sources/engine/GameEngine/GameEngine.hpp
     std::list<GameView *>     _gameviewList;
-=======
-    MyGLPainter		_painter;
-    Camera		_cam;
-    Model		*_model; // représente l'ensemble du monde
-    Material		_mat;
-    std::vector<Mesh>	_mesh;
->>>>>>> origin/master:sources/engine/gameengine.h
+
 };
 
 #endif // GAMEENGINE_H
