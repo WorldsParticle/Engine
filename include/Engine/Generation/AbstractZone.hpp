fc8b532875ed07c5b2653ca1588684f50b83412a
thibaud-fdb <thibaud.fdb@gmail.com>
2016-02-23 16:19:51 +0300

    added some basic code

--- include/Engine/Generation/AbstractZone.hpp
+++ include/Engine/Generation/AbstractZone.hpp
@@ -1,6 +1,9 @@
 #ifndef ABSTRACTZONE_HPP
 #define ABSTRACTZONE_HPP
 
+#include <list>
+#include <vector>
+
 namespace   Engine
 {
 
@@ -9,6 +12,52 @@ namespace   Engine
 ///
 class AbstractZone
 {
+public:
+    enum    ZoneType
+    {
+        SEA = 0,
+        LAKE,
+        PLAIN,
+        HILL,
+        MOUTAIN,
+        FOREST
+    };
+
+    ///
+    /// \brief AbstractZone
+    /// \param depth
+    ///
+    AbstractZone(ZoneType type, AbstractZone *parent, unsigned int depth);
+
+    ///
+    /// \brief ~AbstractZone
+    ///
+    virtual ~AbstractZone();
+
+    ///
+    /// \brief generate the zone, the childs zone, construct the heightmap and fill it with assets.
+    ///
+    virtual void    generate() = 0;
+
+protected:
+    ///
+    /// \brief _type
+    ///
+    ZoneType                    _type;
+
+    ///
+    /// \brief _parent. if NULL then this zone is the main one.
+    ///
+    AbstractZone*               _parent;
+
+    ///
+    /// \brief _depth
+    ///
+    unsigned int                _depth;
+    ///
+    /// \brief _childs is a 2D map. if _childs[x + (depth+1) * y] = NULL, this zone is generated
+    ///
+    std::vector<AbstractZone*> _childs;
 };
 
 }
--- source/Generation/AbstractZone.cpp
+++ source/Generation/AbstractZone.cpp
@@ -1,1 +1,31 @@
 #include "Engine/Generation/AbstractZone.hpp"
+
+#include <math.h>
+
+namespace Engine
+{
+
+AbstractZone::AbstractZone(ZoneType type, AbstractZone *parent, unsigned int depth) :
+    _type(type),
+    _parent(parent),
+    _depth(depth),
+    _childs()
+{
+    for (int i = 0; i < pow(_depth + 1, 2); ++i)
+        _childs.push_back(nullptr);
+}
+
+AbstractZone::~AbstractZone()
+{
+    for (int i = 0; i <= _depth; ++i)
+    {
+        for (int j = 0; j <= _depth; ++j)
+        {
+            if (_childs.at(i + j * (_depth+1)))
+                delete (_childs[i + j * (_depth+1)]);
+        }
+    }
+    _childs.clear();
+}
+
+}
